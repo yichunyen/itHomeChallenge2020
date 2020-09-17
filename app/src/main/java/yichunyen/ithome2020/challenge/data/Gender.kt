2b4d10cdc016143143c04b60352d2efcb3936c63
yichunyen <yenyichun.developer@gmail.com>
2020-09-17 23:11:21 +0800

    [data] Add Gender enum to show all gender type in Profile

--- app/src/main/java/yichunyen/ithome2020/challenge/data/Gender.kt
+++ app/src/main/java/yichunyen/ithome2020/challenge/data/Gender.kt
@@ -1,4 +1,11 @@
 package yichunyen.ithome2020.challenge.data
 
-enum class Gender {
+/**
+ * the gender enum for profile
+ */
+enum class Gender(val type: String) {
+    MALE("male"),
+    FEMALE("female"),
+    UNKNOWN("unknown"),
+    NONE("n/a")
 }
