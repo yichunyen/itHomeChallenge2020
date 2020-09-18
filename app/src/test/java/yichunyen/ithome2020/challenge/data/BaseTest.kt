0fc6f32fb46b7c2b33154a92c032ed4aab050a3b
yichunyen <yenyichun.developer@gmail.com>
2020-09-18 23:05:11 +0800

    [test] Add BaseTest to read the json file from assets foloder

---
+++ app/src/test/java/yichunyen/ithome2020/challenge/data/BaseTest.kt
@@ -0,0 +1,29 @@
+package yichunyen.ithome2020.challenge.data
+
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
+private const val ASSET_BASE_PATH = "../app/src/main/assets/"
+
+open class BaseTest {
+    fun readJsonFile(jsonFileName: String): String {
+        if (jsonFileName.endsWith("json")) {
+            throw Exception("Do not put the json file extension")
+        }
+
+        val bufferedReader = BufferedReader(
+            InputStreamReader(
+                FileInputStream("${ASSET_BASE_PATH}$jsonFileName.json")
+            )
+        )
+        val stringBuilder = StringBuilder()
+        var line = bufferedReader.readLine()
+        while (line != null) {
+            stringBuilder.append(line)
+            line = bufferedReader.readLine()
+        }
+
+        return stringBuilder.toString()
+    }
+}
