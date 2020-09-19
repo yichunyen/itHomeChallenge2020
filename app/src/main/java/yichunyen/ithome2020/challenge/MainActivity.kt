3f986a366639bdd1e4a70d1f38b22406370d545b
yichunyen <yenyichun.developer@gmail.com>
2020-09-19 22:45:57 +0800

    [feature] Fetch start wars people API via NetworkManager

--- app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
+++ app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
@@ -1,11 +1,32 @@
 package yichunyen.ithome2020.challenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import yichunyen.ithome2020.challenge.data.ProfileList
+import yichunyen.ithome2020.challenge.network.NetworkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Call profile list api
+        val call = NetworkManager.client.profileList()
+        call.enqueue(object : Callback<ProfileList> {
+            override fun onResponse(call: Call<ProfileList>, response: Response<ProfileList>) {
+                response.body()?.let {
+                    it.results.forEach {
+                        Log.i("MainActivity", "$it")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ProfileList>, t: Throwable) {
+
+            }
+        })
     }
 }
