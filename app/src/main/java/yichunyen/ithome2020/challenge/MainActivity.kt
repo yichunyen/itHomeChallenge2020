d9e8949cda9f458558708f0095ad6f0ef4f68983
yichunyen <yenyichun.developer@gmail.com>
2020-09-20 21:23:59 +0800

    [feature] MainActivity: implement MVP architecture via MainPresenter.

--- app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
+++ app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
@@ -3,30 +3,33 @@ package yichunyen.ithome2020.challenge
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import yichunyen.ithome2020.challenge.data.ProfileList
-import yichunyen.ithome2020.challenge.network.NetworkManager
+import yichunyen.ithome2020.challenge.data.Profile
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+    private var presenter: MainContract.Presenter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Call profile list api
-        val call = NetworkManager.client.profileList()
-        call.enqueue(object : Callback<ProfileList> {
-            override fun onResponse(call: Call<ProfileList>, response: Response<ProfileList>) {
-                response.body()?.let {
-                    it.results.forEach {
-                        Log.i("MainActivity", "$it")
-                    }
-                }
-            }
+        MainPresenter(this)
+        presenter?.fetchData()
+    }
 
-            override fun onFailure(call: Call<ProfileList>, t: Throwable) {
+    override fun onStop() {
+        super.onStop()
+        presenter?.cancelAPIRequest()
+    }
 
-            }
-        })
+    override fun showProfileList(list: List<Profile>) {
+        list.forEach {
+            Log.i("MainActivity", "$it")
+        }
+    }
+
+    override fun showApiError(errorMessage: String) {
+        Log.e("MainActivity", errorMessage)
+    }
+
+    override fun setPresenter(presenter: MainContract.Presenter) {
+        this.presenter = presenter
     }
 }
