0981810b01ac3a6fa87da62196c564406feb0c8f
yichunyen <yenyichun.developer@gmail.com>
2020-09-24 23:03:29 +0800

    [feature] MainActivity: show the custom toolbar

--- app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
+++ app/src/main/java/yichunyen/ithome2020/challenge/MainActivity.kt
@@ -1,5 +1,6 @@
 package yichunyen.ithome2020.challenge
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +13,16 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
         MainPresenter(this)
         presenter?.fetchData()
+    }
+
+    private fun initToolbar() {
+        toolbar.setTitle(R.string.app_name)
+        toolbar.setTitleTextColor(Color.BLACK)
+        toolbar.setBackgroundResource(R.color.colorPrimary)
+        setSupportActionBar(toolbar)
     }
 
     override fun onStop() {
